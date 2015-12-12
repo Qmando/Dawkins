38d9102c7247e07803e5a69f4a00569cf2e42993
Jimmy Tang <jimmyt@yelp.com>
2015-12-12 01:57:57 -0800

    added some deletion logic

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -19,9 +19,10 @@ public class NewBehaviourScript : MonoBehaviour {
 	public GameObject[] InstanceTypes { get; set; }
 
 	void Start() { 
-		Density = 100;
-		SpawnArea = new Rect (0, 0, 5.0f, 5.0f);
-		SpawnClearArea = new Rect (0, 0, 5.0f / 2, 5.0f / 2);
+		Density = 20;
+		var w = 40.0f;
+		SpawnArea = new Rect (-w / 2, -w / 2, w, w);
+		SpawnClearArea = new Rect (SpawnArea.xMin / 2, SpawnArea.yMin / 2, SpawnArea.width / 2, SpawnArea.height / 2);
 		player = GameObject.Find ("Cell");
 		InstanceTypes = new GameObject[] { 
 			GameObject.Find("flagella")
@@ -32,27 +33,42 @@ public class NewBehaviourScript : MonoBehaviour {
 
 	System.Random rand = new System.Random();
 		
-	GameObject CreateCreature() { 
+	GameObject CreateCreature(bool avoidNearby = true) { 
 		var creature = Instantiate (InstanceTypes [rand.Next() % InstanceTypes.Length]);
 		creature.tag = "creature";
-		creature.transform.position = player.transform.position + new Vector3(
-			(float)rand.NextDouble () * (float)SpawnArea.width - SpawnArea.width / 2,
-			(float)rand.NextDouble () * (float)SpawnArea.height - SpawnArea.height / 2,
-			0);
+		creature.transform.Rotate (Vector3.forward, (float)rand.NextDouble () * 360);
+		creature.transform.position = player.transform.position + genOffset (avoidNearby);
 		Debug.LogWarning (new { Creature = creature.transform.position });		
 		return creature;
 	}
 
+	Vector3 genOffset(bool avoidNearby) { 
+		Vector3 v = Vector3.zero;
+		do { 
+			v = new Vector3 (
+				(float)rand.NextDouble () * (float)SpawnArea.width + SpawnArea.xMin,
+				(float)rand.NextDouble () * (float)SpawnArea.height + SpawnArea.yMin,
+				0);
+		} while(avoidNearby && SpawnClearArea.Contains (v)) ;
+		return v;
+	}
 	IEnumerator dostuff() { 
 		// start by initially populating the space around the current Player
 		for (int i = 0; i < Density; i++)
-			CreateCreature ();
+			CreateCreature (false);
 
 		while (true) { 
 			for (int i = GameObject.FindGameObjectsWithTag ("creature").Length; i < Density; i++)
 				CreateCreature ();
 
-			Debug.LogWarning (new { Player = player.transform.position });
+			var to_delete = 
+				from x in GameObject.FindGameObjectsWithTag ("creature")
+				where Vector3.Distance (x.transform.position, player.transform.position) > 15
+				select x;
+
+			foreach (var x in to_delete)
+				GameObject.Destroy (x);
+
 			yield return 0;
 		}
 	}
