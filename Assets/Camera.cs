fcaef21ee1cc106c8da7b77999e795a479253881
Quentin Long <qlo@yelp.com>
2015-12-11 23:46:11 -0800

    Dawkins

---
+++ Assets/Camera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+	public Transform follow;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = follow.position + new Vector3 (0, 0, -10);
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public Transform body;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	Vector2 mouseOffset () {
+		float x = Input.mousePosition.x - Screen.currentResolution.width/2;
+		float y = Input.mousePosition.y - Screen.currentResolution.height/2;
+		return new Vector2 (x, y);
+	}
+
+	void Move () {
+		body.transform.Translate (mouseOffset () * speed * Time.deltaTime);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Move ();
+		
+	}
+}
